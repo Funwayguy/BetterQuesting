5d5d2aea01ae9164d02b084559fe7a0b0a728261
Funwayguy <FunwayguyEmail@gmail.com>
2016-12-30 10:53:15 +1000

    Quick fix for quests not opening

--- src/main/java/betterquesting/client/gui/GuiQuestLinesMain.java
+++ src/main/java/betterquesting/client/gui/GuiQuestLinesMain.java
@@ -173,9 +173,12 @@ public class GuiQuestLinesMain extends GuiScreenThemed implements INeedsRefresh
 		
 		if(tree != null)
 		{
-			GuiButtonQuestInstance btn = tree.getButtonAt(qlGui.getRelativeX(mx), qlGui.getRelativeY(my));
+			int rmx = qlGui.getRelativeX(mx);
+			int rmy = qlGui.getRelativeY(my);
 			
-			if(btn != null && btn.mousePressed(mc, mx, my))
+			GuiButtonQuestInstance btn = tree.getButtonAt(rmx, rmy);
+			
+			if(btn != null && btn.mousePressed(mc, rmx, rmy))
 			{
 				btn.func_146113_a(mc.getSoundHandler());
 				bookmarked = new GuiQuestInstance(this, btn.getQuest());
