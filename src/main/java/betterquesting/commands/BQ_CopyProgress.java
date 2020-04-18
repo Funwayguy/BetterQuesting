3b6581986e212048ea6005c684778eb9498e4fcc
SKProCH <shadowkillerpropro@yandex.ru>
2020-04-18 15:41:13 +0300

    Rework command for console execution

--- src/main/java/betterquesting/commands/BQ_CopyProgress.java
+++ src/main/java/betterquesting/commands/BQ_CopyProgress.java
@@ -5,16 +5,21 @@ import betterquesting.api2.storage.DBEntry;
 import betterquesting.network.handlers.NetQuestSync;
 import betterquesting.questing.QuestDatabase;
 import com.google.common.collect.Lists;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerProfileCache;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.UsernameCache;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -49,28 +54,76 @@ public class BQ_CopyProgress extends CommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "/bq_copyquests <toPlayer> [fromPlayer]";
+        return "/bq_copyquests <toPlayer> |OR| /bq_copyquests <fromPlayer> <toPlayer>";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        if(sender instanceof EntityPlayer) {
-            if(args.length == 0 || args.length > 2) throw new CommandException("Wrong arguments");
-            UUID ownUUID = (args.length == 2 ? getPlayer(sender, args[1]) : ((EntityPlayer) sender)).getPersistentID();
-            EntityPlayerMP addPlayer = getPlayer(sender, args[0]);
-            UUID addUUID = addPlayer.getPersistentID();
-            long current = System.currentTimeMillis();
-            List<Integer> ids = new ArrayList<>();
-            for(DBEntry<IQuest> questDBEntry : QuestDatabase.INSTANCE.getEntries()) {
-                IQuest quest = questDBEntry.getValue();
-                if(quest.isComplete(ownUUID) && !quest.isComplete(addUUID)) {
-                    quest.setComplete(addUUID, current);
-                    ids.add(questDBEntry.getID());
-                }
-            }
+        if(args.length == 0 || args.length > 2) {
+            throw new CommandException("Wrong arguments");
+        }
 
-            NetQuestSync.sendSync(addPlayer, ids.stream().mapToInt(i -> i).toArray(), false, true);
-            sender.addChatMessage(new ChatComponentText("Completed " + ids.size() + " for " + addUUID));
+        UUID fromUUID;
+        UUID toUUID;
+        if (args.length == 2)
+        {
+            fromUUID = GetPlayerUUID(args[0]);
+            toUUID = GetPlayerUUID(args[1]);
+        }else if(sender instanceof EntityPlayer){
+            fromUUID = ((EntityPlayer) sender).getPersistentID();
+            toUUID = GetPlayerUUID(args[0]);
+        }else {
+            throw new CommandException("Wrong arguments");
+        }
+
+        System.out.println(fromUUID.toString());
+        System.out.println(toUUID.toString());
+
+        long current = System.currentTimeMillis();
+        List<Integer> ids = new ArrayList<>();
+        for(DBEntry<IQuest> questDBEntry : QuestDatabase.INSTANCE.getEntries()) {
+            IQuest quest = questDBEntry.getValue();
+            if(quest.isComplete(fromUUID) && !quest.isComplete(toUUID)) {
+                quest.setComplete(toUUID, current);
+                ids.add(questDBEntry.getID());
+            }
+        }
+
+        EntityPlayerMP player = getPlayerAdvanced(sender, toUUID.toString());
+        if (player != null){
+            NetQuestSync.sendSync(player, ids.stream().mapToInt(i -> i).toArray(), false, true);
+        }
+
+        sender.addChatMessage(new ChatComponentText("Completed " + ids.size() + " for " + toUUID));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static EntityPlayerMP getPlayerAdvanced(ICommandSender p_82359_0_, String p_82359_1_){
+        Optional onlinePlayer = MinecraftServer.getServer().getConfigurationManager().playerEntityList.stream().filter(i -> i instanceof EntityPlayerMP)
+                .filter(o -> ((EntityPlayerMP) o).getPersistentID().toString().equals(p_82359_1_) || ((EntityPlayerMP) o).getDisplayName().equals(p_82359_1_)).findFirst();
+        try {
+            return onlinePlayer.isPresent() ? (EntityPlayerMP) onlinePlayer.get() : getPlayer(p_82359_0_, p_82359_1_);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    private static UUID GetPlayerUUID(String data){
+        try {
+            return UUID.fromString(data);
+        } catch (IllegalArgumentException e) {
+            Optional onlinePlayer = MinecraftServer.getServer().getConfigurationManager().playerEntityList.stream().filter(i -> i instanceof EntityPlayerMP)
+                    .filter(o -> ((EntityPlayerMP) o).getPersistentID().toString().equals(data) || ((EntityPlayerMP) o).getDisplayName().equals(data)).findFirst();
+            if (onlinePlayer.isPresent())
+                return ((EntityPlayerMP)onlinePlayer.get()).getPersistentID();
+
+            GameProfile gameProfile = new PlayerProfileCache(MinecraftServer.getServer(), MinecraftServer.field_152367_a).func_152655_a(data);
+            if (gameProfile != null)
+                return gameProfile.getId();
+
+            return UUID.nameUUIDFromBytes(data.getBytes(StandardCharsets.UTF_8));
         }
     }
 }
