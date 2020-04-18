1728e50ca36a70b387b7e73ebb1672b5fe246a66
SKProCH <shadowkillerpropro@yandex.ru>
2020-04-18 14:21:12 +0300

    Add tab completion options

--- src/main/java/betterquesting/commands/BQ_CopyProgress.java
+++ src/main/java/betterquesting/commands/BQ_CopyProgress.java
@@ -2,31 +2,39 @@ package betterquesting.commands;
 
 import betterquesting.api.questing.IQuest;
 import betterquesting.api2.storage.DBEntry;
-import betterquesting.network.PacketSender;
 import betterquesting.network.handlers.NetQuestSync;
 import betterquesting.questing.QuestDatabase;
-import betterquesting.questing.QuestInstance;
-import betterquesting.storage.NameCache;
 import com.google.common.collect.Lists;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.network.play.server.S2BPacketChangeGameState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BQ_CopyProgress extends CommandBase {
     @Override
     public int getRequiredPermissionLevel() {
         return 2;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
+        if (p_71516_2_.length > 2)
+            return null;
+
+        String s = p_71516_2_.length != 0 ? p_71516_2_[p_71516_2_.length - 1] : "";
+        return ((Stream<EntityPlayerMP>)MinecraftServer.getServer().getConfigurationManager().playerEntityList.stream().filter(i -> i instanceof EntityPlayerMP))
+                .filter(o -> o.getDisplayName().startsWith(s))
+                .map(EntityPlayer::getDisplayName).collect(Collectors.toList());
     }
 
     @Override
