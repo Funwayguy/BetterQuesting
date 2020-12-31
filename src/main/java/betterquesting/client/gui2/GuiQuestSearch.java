98dc52c3fcef93457e8fbc298c4137b6c83b1443
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2020-12-31 13:49:41 +0100

    Cleanup

--- src/main/java/betterquesting/client/gui2/GuiQuestSearch.java
+++ src/main/java/betterquesting/client/gui2/GuiQuestSearch.java
@@ -34,16 +34,42 @@ public class GuiQuestSearch extends GuiScreenCanvas {
         CanvasEmpty cvInner = new CanvasEmpty(new GuiTransform(GuiAlign.FULL_BOX, new GuiPadding(8, 8, 8, 8), 0));
         cvBackground.addPanel(cvInner);
 
-        PanelButton btnExit = new PanelButton(new GuiTransform(GuiAlign.BOTTOM_CENTER, -100, -16, 200, 16, 0), 0, QuestTranslation.translate("gui.back"));
-        btnExit.setClickAction((b) -> mc.displayGuiScreen(parent));
-        cvInner.addPanel(btnExit);
+        createExitButton(cvInner);
 
         PanelTextBox txtDb = new PanelTextBox(new GuiTransform(GuiAlign.TOP_EDGE, new GuiPadding(0, 0, 0, -16), 0), QuestTranslation.translate("betterquesting.gui.search")).setAlignment(1).setColor(PresetColor.TEXT_MAIN.getColor());
         cvInner.addPanel(txtDb);
 
+        createSearchBox(cvInner);
+    }
+
+
+
+    private void createExitButton(CanvasEmpty cvInner) {
+        PanelButton btnExit = new PanelButton(
+                new GuiTransform(GuiAlign.BOTTOM_CENTER, -100, -16, 200, 16, 0),
+                0,
+                QuestTranslation.translate("gui.back")
+        );
+        btnExit.setClickAction((b) -> mc.displayGuiScreen(parent));
+        cvInner.addPanel(btnExit);
+    }
+
+    private void createSearchBox(CanvasEmpty cvInner) {
         PanelTextField<String> searchBox = new PanelTextField<>(new GuiTransform(GuiAlign.TOP_EDGE, new GuiPadding(0, 16, 8, -32), 0), "", FieldFilterString.INSTANCE);
         searchBox.setWatermark("Search...");
         cvInner.addPanel(searchBox);
+
+        CanvasQuestSearch canvasQuestSearch = createSearchCanvas();
+        cvInner.addPanel(canvasQuestSearch);
+
+        searchBox.setCallback(canvasQuestSearch::setSearchFilter);
+
+        PanelVScrollBar scDb = new PanelVScrollBar(new GuiTransform(GuiAlign.RIGHT_EDGE, new GuiPadding(-8, 32, 0, 24), 0));
+        cvInner.addPanel(scDb);
+        canvasQuestSearch.setScrollDriverY(scDb);
+    }
+
+    private CanvasQuestSearch createSearchCanvas(){
 
         CanvasQuestSearch canvasQuestSearch = new CanvasQuestSearch(new GuiTransform(GuiAlign.FULL_BOX, new GuiPadding(0, 32, 8, 24), 0), mc.thePlayer);
         canvasQuestSearch.setQuestOpenCallback(questSearchEntry -> {
@@ -55,13 +81,7 @@ public class GuiQuestSearch extends GuiScreenCanvas {
             mc.displayGuiScreen(parent);
             acceptCallback(questSearchEntry);
         });
-        cvInner.addPanel(canvasQuestSearch);
-
-        searchBox.setCallback(canvasQuestSearch::setSearchFilter);
-
-        PanelVScrollBar scDb = new PanelVScrollBar(new GuiTransform(GuiAlign.RIGHT_EDGE, new GuiPadding(-8, 32, 0, 24), 0));
-        cvInner.addPanel(scDb);
-        canvasQuestSearch.setScrollDriverY(scDb);
+        return canvasQuestSearch;
     }
 
     private Consumer<QuestSearchEntry> callback;
