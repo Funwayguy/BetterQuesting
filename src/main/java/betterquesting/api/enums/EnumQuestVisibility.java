aa2a86a5f8f4504064c702e7a249df28c1c37b71
D-Cysteine <54219287+D-Cysteine@users.noreply.github.com>
2021-10-22 03:40:10 -0600

    Update comment

--- src/main/java/betterquesting/api/enums/EnumQuestVisibility.java
+++ src/main/java/betterquesting/api/enums/EnumQuestVisibility.java
@@ -2,7 +2,7 @@ package betterquesting.api.enums;
 
 public enum EnumQuestVisibility
 {
-	HIDDEN, // Never shown.
+	HIDDEN, // Never shown, and hidden from view mode.
 	SECRET, // Like UNLOCKED, except also hidden from view mode.
 	UNLOCKED, // Must be unlocked to be shown (all prerequisites must be completed).
 	NORMAL, // Will be shown if all prerequisites are unlocked.
