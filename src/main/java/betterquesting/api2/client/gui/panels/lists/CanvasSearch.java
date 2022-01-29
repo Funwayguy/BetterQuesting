8e88d1bd813399cea294b371956e6861fab276b1
ALongStringOfNumbers <sjensen793@gmail.com>
2022-01-28 22:43:25 -0700

    Minor formatting

--- src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasSearch.java
+++ src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasSearch.java
@@ -6,8 +6,8 @@ import com.google.common.base.Stopwatch;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public abstract class CanvasSearch<T, E> extends CanvasScrolling
-{
+public abstract class CanvasSearch<T, E> extends CanvasScrolling {
+
     private String searchTerm = "";
     private Iterator<E> searching = null;
     private final Stopwatch searchTime = Stopwatch.createStarted();
@@ -15,36 +15,31 @@ public abstract class CanvasSearch<T, E> extends CanvasScrolling
     private int searchIdx = 0; // Where are we in the ongoing search?
     private final ArrayDeque<T> pendingResults = new ArrayDeque<>();
     private final List<T> savedResults = new ArrayList<>();
-    
-    public CanvasSearch(IGuiRect rect)
-    {
+
+    public CanvasSearch(IGuiRect rect) {
         super(rect);
     }
-    
-    public void setSearchFilter(String text)
-    {
+
+    public void setSearchFilter(String text) {
         this.searchTerm = text.toLowerCase();
         refreshSearch();
     }
-    
+
     @Override
-    public void initPanel()
-    {
+    public void initPanel() {
         super.initPanel();
         refreshSearch();
     }
-    
+
     @Override
-    public void drawPanel(int mx, int my, float partialTick)
-    {
+    public void drawPanel(int mx, int my, float partialTick) {
         updateSearch();
         updateResults();
-        
+
         super.drawPanel(mx, my, partialTick);
     }
-    
-    public void refreshSearch()
-    {
+
+    public void refreshSearch() {
         this.resetCanvas();
         this.searchIdx = 0;
         this.searching = getIterator();
@@ -52,60 +47,53 @@ public abstract class CanvasSearch<T, E> extends CanvasScrolling
         this.pendingResults.clear();
         this.savedResults.clear();
     }
-    
-    private void updateSearch()
-    {
-        if(searching == null)
-        {
+
+    private void updateSearch() {
+        if (searching == null) {
             return;
-        } else if(!searching.hasNext())
-        {
+        } else if (!searching.hasNext()) {
             searching = null;
             return;
         }
-    
+
         searchTime.reset().start();
-        
+
         ArrayDeque<T> tmp = new ArrayDeque<>();
-        while(searching.hasNext() && searchTime.elapsed(TimeUnit.MILLISECONDS) < 10)
-        {
+        while (searching.hasNext() && searchTime.elapsed(TimeUnit.MILLISECONDS) < 10) {
             E entry = searching.next();
-            
-            if(entry != null)
-            {
+
+            if (entry != null) {
                 queryMatches(entry, searchTerm, tmp);
             }
         }
-        
+
         pendingResults.addAll(tmp);
         savedResults.addAll(tmp);
-        
+
         searchTime.stop();
     }
-    
-    private void updateResults()
-    {
-        if(pendingResults.isEmpty())
-        {
+
+    private void updateResults() {
+        if (pendingResults.isEmpty()) {
             return;
         }
-        
+
         searchTime.reset().start();
-        
-        while(!pendingResults.isEmpty() && searchTime.elapsed(TimeUnit.MILLISECONDS) < 100)
-        {
-            if(addResult(pendingResults.poll(), searchIdx, resultWidth)) searchIdx++;
+
+        while (!pendingResults.isEmpty() && searchTime.elapsed(TimeUnit.MILLISECONDS) < 100) {
+            if (addResult(pendingResults.poll(), searchIdx, resultWidth)) searchIdx++;
         }
-        
+
         searchTime.stop();
     }
-    
-    public List<T> getResults()
-    {
+
+    public List<T> getResults() {
         return Collections.unmodifiableList(savedResults);
     }
-    
+
     protected abstract Iterator<E> getIterator();
+
     protected abstract void queryMatches(E value, String query, final ArrayDeque<T> results);
+
     protected abstract boolean addResult(T entry, int index, int cachedWidth);
 }
